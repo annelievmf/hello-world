9d0cfd2055dd3a65e8206bf6507e832ccbb9fa6b
Annelie von Matérn Fallkvist <anneliefallkvist@Annelies-MacBook-Pro-10.local>
2019-10-09 11:59:03 +0200

    initial commit

---
+++ precache-manifest.8eaeef733f376d5deaad4b68cd314f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e1299d5760959f3f7a4374ac9b94e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3633b46a3e9a976ed9f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2bda56ce5612f2b42795",
+    "url": "/static/js/2.b7d8417b.chunk.js"
+  },
+  {
+    "revision": "c3633b46a3e9a976ed9f",
+    "url": "/static/js/main.384208f2.chunk.js"
+  },
+  {
+    "revision": "5e01aee4373a34d38332",
+    "url": "/static/js/runtime-main.4f3c918c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
